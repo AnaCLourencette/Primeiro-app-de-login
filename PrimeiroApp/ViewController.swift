98d0d455239c9ef550d1e58eb582994aa7818e91
Ana Carol <carol.lourencette@hotmail.com>
2023-04-30 18:51:46 -0300

    adicionando o projeto

---
+++ PrimeiroApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PrimeiroApp
+//
+//  Created by Carol Lourencette on 30/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Meu primeiro App")
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
